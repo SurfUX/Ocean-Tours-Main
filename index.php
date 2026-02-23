e6a99f0b311ec47d0b01cd00784fc0be2516efd7
SurfUX <tecnetsoftwaresolution@gmail.com>
2026-02-23 18:06:40 +0530

    Index Name Changes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-    <title>Index - Tour Bootstrap Template</title>
+    <title>Great Ocean Tours</title>
     <meta name="description" content="" />
     <meta name="keywords" content="" />
 
